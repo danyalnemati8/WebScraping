bd435163336a6d6acb6e464949b977dfcd30cc5a
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-07-02 15:44:55 -0700

    imported library and got full page contents from newegg

--- webscrap.py
+++ webscrap.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f,"html.parser")
-tags = doc.find_all("p")
-print(tags)
+import requests
+import re
+
+url = "https://www.newegg.com/Newegg-Deals/EventSaleStore/ID-9447?N=100006662"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+prices = doc.find_all(text=re.compile("\$.*"))
+
+print(prices)
 
