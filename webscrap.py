c7e3e5017f2287de23a07bc715474d375c3e74a2
danyalnemati8 <dnemati@csu.fullerton.edu>
2023-07-03 12:35:21 -0700

    good formatting

--- webscrap.py
+++ webscrap.py
@@ -1,27 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+
 count = 1
 n = 1
+
 while count <= 3:
     url = f"https://www.newegg.com/p/pl?N=100007709%204131%20601357247%204814&d=3080&PageSize=36&isdeptsrh=1&page={count}"
     
-    
-    print("count = ", end="")
-    print(count)
+    print("count =", count)
     print(url)
 
     result = requests.get(url).text
     doc = BeautifulSoup(result, "html.parser")
 
-    prices = doc.find_all(["li", "strong"], class_="price-current")
-    count = count + 1
+    prices = doc.find_all("li", class_="price-current")
+    count += 1
     
     for price in prices:
-        print("$" + price.find("strong").text, end="")
-        print(price.find("sup").text)
-        print("result number = ", end="")
-        print(n)
-        n = n + 1
+        strong = price.find("strong")
+        sup = price.find("sup")
+        
+        if strong and sup:
+            print("$" + strong.text, end="")
+            print(sup.text)
+        else:
+            print("Price information not found")
+
+        print("result number =", n)
+        n += 1
         print("\n")
-    
