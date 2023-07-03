3c4df390c5a3fff791cc5a6b9490a8a37ae7762e
danyalnemati8 <dnemati@csu.fullerton.edu>
2023-07-03 12:50:36 -0700

    list

--- webscrap.py
+++ webscrap.py
@@ -5,9 +5,12 @@ import re
 count = 1
 n = 1
 
+prices_list = []  # Empty list to store prices
+result_numbers_list = []  # Empty list to store result numbers
+
 while count <= 3:
     url = f"https://www.newegg.com/p/pl?N=100007709%204131%20601357247%204814&d=3080&PageSize=36&isdeptsrh=1&page={count}"
-    
+
     print("count =", count)
     print(url)
 
@@ -22,11 +25,17 @@ while count <= 3:
         sup = price.find("sup")
         
         if strong and sup:
-            print("$" + strong.text, end="")
-            print(sup.text)
+            price_value = strong.text + sup.text
+            prices_list.append(price_value)
+            result_numbers_list.append(n)
+            print("$" + price_value)
         else:
             print("Price information not found")
 
-        print("result number =", n)
         n += 1
+        print("result number =", n)
         print("\n")
+
+# Print the final lists
+print("Prices:", prices_list)
+print("Result Numbers:", result_numbers_list)
