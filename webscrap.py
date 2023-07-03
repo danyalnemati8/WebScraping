c1f005d65a7e62e6d0d20613d50666f947e3ec8b
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-07-03 11:05:38 -0700

    added newline spacing to make it easier to read and simple filtering to get parent tag of the information we actually want to scrape

--- webscrap.py
+++ webscrap.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://www.newegg.com/Newegg-Deals/EventSaleStore/ID-9447?N=100006662"
+url = "https://www.newegg.com/p/pl?N=100007709%204131&d=3080&isdeptsrh=1"
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+result = requests.get(url).text
+doc = BeautifulSoup(result, "html.parser")
 
-prices = doc.find_all(text=re.compile("\$.*"))
+prices = doc.find_all(["li", "strong"], class_="price-current")
 
-print(prices)
-
+for price in prices:
+    print(price)
+    print("\n")
+    
