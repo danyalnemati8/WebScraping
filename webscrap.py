4041fe22efa0fff59faacf3a4943e9a9453ea9bc
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-07-03 11:28:47 -0700

    filtered all page now for full results

--- webscrap.py
+++ webscrap.py
@@ -1,19 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-
-url = "https://www.newegg.com/p/pl?N=100007709%204131%20601357247&d=3080&page=1&isdeptsrh=1"
-
-result = requests.get(url).text
-doc = BeautifulSoup(result, "html.parser")
-
-prices = doc.find_all(["li", "strong"], class_="price-current")
+count = 1
 n = 0
-for price in prices:
-    print(price.find("strong"))
-    print(price.find("sup"))
-    print("result number = ", end="")
-    print(n)
-    n = n + 1
-    print("\n")
+while count <= 4:
+    url = "https://www.newegg.com/p/pl?N=100007709%204131%20601357247&d=3080&page={count}&isdeptsrh=1"
+
+    result = requests.get(url).text
+    doc = BeautifulSoup(result, "html.parser")
+
+    prices = doc.find_all(["li", "strong"], class_="price-current")
+    count = count + 1
+    
+    for price in prices:
+        print(price.find("strong"))
+        print(price.find("sup"))
+        print("result number = ", end="")
+        print(n)
+        n = n + 1
+        print("\n")
     
