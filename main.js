8c274e343a46900ab2c784a382778e945bdad156
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-07-11 12:52:58 -0700

    added basic graph in index.html

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let FULL_URL = 'https://docs.google.com/spreadsheets/d/1Bzedq9WqMuvgaV9VdpGXVJ4kXEjC5iVpslgnMFNYMkk/edit#gid=0';
 
 fetch(FULL_URL)
@@ -11,22 +10,47 @@ fetch(FULL_URL)
     // Use DOM manipulation to extract data from the parsed HTML
     const tableRows = tempElement.querySelectorAll('table tr');
 
-   // Process the extracted data as needed
-   tableRows.forEach(row => {
-    const cells = row.querySelectorAll('td');
-    const rowData = Array.from(cells)
-      .map(cell => cell.textContent);
+    // Process the extracted data as needed
+    const labels = [];
+    const datasets = [];
+    tableRows.forEach(row => {
+      const cells = row.querySelectorAll('td');
+      const rowData = Array.from(cells)
+        .map(cell => cell.textContent);
 
-    // Process the rowData until an empty cell is encountered
-    
- // Plot 3070 data for now 
-      const lastIndex = 8;
-      const filteredData = rowData.slice(4, lastIndex).filter(cellData => cellData.trim() !== '');
+      // Filter out empty cells
+      const nonEmptyData = rowData.slice(4, 8).filter(cellData => cellData.trim() !== '');
 
-      console.log(filteredData);
+      if (nonEmptyData.length > 0) {
+        labels.push(rowData[0]); // Use the value from column A as the label
+        datasets.push({
+          label: 'Data',
+          data: nonEmptyData,
+          backgroundColor: 'rgba(0, 123, 255, 0.5)'
+        });
+      }
+    });
+
+    // Create a new Chart.js chart
+    const ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: datasets
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
     });
   })
   .catch(error => {
     console.error(error);
   });
+
 
