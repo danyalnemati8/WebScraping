d042ecbeb2cb51a363daae766b44c9e57361926b
Andy Gale <77473114+andy-gale@users.noreply.github.com>
2023-07-11 18:15:29 -0700

    fixed x-axis and change chart style to scatterplot using d3 instead of chart.js

--- main.js
+++ main.js
@@ -1,65 +1,81 @@
 let FULL_URL = 'https://docs.google.com/spreadsheets/d/1Bzedq9WqMuvgaV9VdpGXVJ4kXEjC5iVpslgnMFNYMkk/edit#gid=0';
 
-fetch(FULL_URL)
-  .then(response => response.text())
-  .then(html => {
-    // Create a temporary element to parse the HTML content
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = html;
+    fetch(FULL_URL)
+      .then(response => response.text())
+      .then(html => {
+        const tempElement = document.createElement('div');
+        tempElement.innerHTML = html;
 
-    // Use DOM manipulation to extract data from the parsed HTML
-    const tableRows = tempElement.querySelectorAll('table tr');
+        const tableRows = tempElement.querySelectorAll('table tr');
 
-    // Process the extracted data as needed
-    const labels = [];
-    const data = [];
-    // Start iterating from index 1 to skip the first row
-    for (let i = 2; i < tableRows.length; i++) {
-      const row = tableRows[i];
-      const cells = row.querySelectorAll('td');
-      const cellDataF = cells[5] ? cells[5].textContent : ''; // Data from column F
-      const cellDataG = cells[6] ? cells[6].textContent : ''; // Data from column G
+        const data = [];
+        for (let i = 2; i < tableRows.length; i++) {
+          const row = tableRows[i];
+          const cells = row.querySelectorAll('td');
+          const cellDataF = cells[5] ? cells[5].textContent : '';
+          const cellDataG = cells[6] ? parseFloat(cells[6].textContent) : NaN;
 
-      // Filter out empty cells
-      if (cellDataF.trim() !== '' && cellDataG.trim() !== '') {
-        labels.push(cellDataF);
-        data.push(parseFloat(cellDataG));
-      }
-    }
-      
-
-    // Create a new Chart.js chart
-    const ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Data',
-          data: data,
-          backgroundColor: 'rgba(0, 123, 255, 0.5)'
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Dates'
-            }
-          },
-          y: {
-            title: {
-              display: true,
-              text: 'Prices'
-            },
-            beginAtZero: true
+          if (cellDataF.trim() !== '' && !isNaN(cellDataG)) {
+            data.push({ x: new Date(cellDataF), y: cellDataG });
           }
         }
-      }
-    });
-  })
-  .catch(error => {
-    console.error(error);
-  });
+
+        const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+        const width = 500 - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        const svg = d3.select('body')
+          .append('svg')
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+          .append('g')
+          .attr('transform', `translate(${margin.left},${margin.top})`);
+
+        const xScale = d3.scaleTime()
+          .domain(d3.extent(data, d => d.x))
+          .range([0, width]);
+
+        const yScale = d3.scaleLinear()
+          .domain(d3.extent(data, d => d.y))
+          .range([height, 0]);
+
+        const xAxis = d3.axisBottom(xScale)
+          .ticks(d3.timeDay)
+          .tickFormat(d3.timeFormat('%b %d'));
+
+        const yAxis = d3.axisLeft(yScale);
+
+        svg.append('g')
+          .attr('class', 'x-axis')
+          .attr('transform', `translate(0,${height})`)
+          .call(xAxis);
+
+        svg.append('g')
+          .attr('class', 'y-axis')
+          .call(yAxis);
+
+        svg.selectAll('circle')
+          .data(data)
+          .enter()
+          .append('circle')
+          .attr('cx', d => xScale(d.x))
+          .attr('cy', d => yScale(d.y))
+          .attr('r', 5)
+          .attr('fill', 'rgba(0, 123, 255, 0.5)');
+
+        svg.append('text')
+          .attr('x', width / 2)
+          .attr('y', height + margin.bottom - 10)
+          .attr('text-anchor', 'middle')
+          .text('Dates');
+
+        svg.append('text')
+          .attr('x', -height / 2)
+          .attr('y', -margin.left + 15)
+          .attr('transform', 'rotate(-90)')
+          .attr('text-anchor', 'middle')
+          .text('Prices');
+      })
+      .catch(error => {
+        console.error(error);
+      });
