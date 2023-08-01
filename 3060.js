db6f15c2a75330829fcfdc9970f9e54f7ff017e4
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-08-01 14:10:25 -0700

    public branch

---
+++ 3060.js
@@ -0,0 +1,171 @@
+FULL_URL = 'https://docs.google.com/spreadsheets/d/1Bzedq9WqMuvgaV9VdpGXVJ4kXEjC5iVpslgnMFNYMkk/edit#gid=0';
+
+fetch(FULL_URL)
+  .then(response => response.text())
+  .then(html => {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = html;
+
+    const tableRows = tempElement.querySelectorAll('table tr');
+
+    const data = [];
+    for (let i = 2; i < tableRows.length; i++) {
+      const row = tableRows[i];
+      const cells = row.querySelectorAll('td');
+      const cellDataF = cells[1] ? cells[1].textContent : '';
+      const cellDataG = cells[2] ? parseFloat(cells[2].textContent) : NaN;
+
+      if (cellDataF.trim() !== '' && !isNaN(cellDataG)) {
+        data.push({ x: new Date(cellDataF), y: cellDataG });
+      }
+    }
+
+    const groupedData = groupDataByDate(data);
+    const meanData = calculateMeanData(groupedData);
+    const medianData = calculateMedianData(groupedData);
+
+    const margin = { top: 50, right: 20, bottom: 50, left: 50 };
+    const width = 350 - margin.left - margin.right;
+    const height = 250 - margin.top - margin.bottom;
+
+    const svg = d3.select('body')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const chartWidth = width + margin.left + margin.right;
+    const chartHeight = height + margin.top + margin.bottom;
+
+    svg.append('rect')
+      .attr('x', -margin.left)
+      .attr('y', -margin.top)
+      .attr('width', chartWidth)
+      .attr('height', chartHeight)
+      .attr('rx', 10) // Set the horizontal radius of the rounded corners
+      .attr('ry', 10) 
+      .attr('fill', 'white'); // Set the background color of the chart to white
+
+
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(data, d => d.x))
+      .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.y))
+      .range([height, 0]);
+
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(d3.timeDay)
+      .tickFormat(d3.timeFormat('%b %d'));
+
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', `translate(0,${height})`)
+      .call(xAxis);
+
+    svg.append('g')
+      .attr('class', 'y-axis')
+      .call(yAxis);
+
+    svg.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', d => xScale(d.x))
+      .attr('cy', d => yScale(d.y))
+      .attr('r', 5)
+      .attr('fill', 'rgba(0, 123, 255, 0.5)');
+
+    const line = d3.line()
+      .x(d => xScale(d.x))
+      .y(d => yScale(d.y))
+      .curve(d3.curveMonotoneX);
+
+    svg.append('path')
+      .datum(meanData)
+      .attr('class', 'mean-line')
+      .attr('fill', 'none')
+      .attr('stroke', 'rgba(255, 0, 0, 0.8)')
+      .attr('stroke-width', 2)
+      .attr('d', line);
+
+    svg.append('path')
+      .datum(medianData)
+      .attr('class', 'median-line')
+      .attr('fill', 'none')
+      .attr('stroke', 'rgba(135, 0, 255, 0.8)')
+      .attr('stroke-width', 2)
+      .attr('d', line);
+
+    svg.append('text')
+      .attr('x', width / 2)
+      .attr('y', -10)
+      .attr('text-anchor', 'middle')
+      .style('font-size', '16px')
+      .text('3060 Series');
+
+    svg.append('text')
+      .attr('x', width / 2)
+      .attr('y', height + margin.bottom - 10)
+      .attr('text-anchor', 'middle')
+      .text('Dates');
+
+    svg.append('text')
+      .attr('x', -height / 2)
+      .attr('y', -margin.left + 15)
+      .attr('transform', 'rotate(-90)')
+      .attr('text-anchor', 'middle')
+      .text('Prices');
+
+      const legend = svg.append('g')
+      .attr('class', 'legend')
+      .attr('transform', `translate(${width - 120}, 10)`); // Updated position
+    
+    legend.append('text')
+      .attr('x', 10)
+      .attr('y', 0)
+      .style('font-size', '12px')
+      .text('Mean');
+    
+    legend.append('line')
+      .attr('x1', -5)
+      .attr('y1', -5)
+      .attr('x2', 8)
+      .attr('y2', -5)
+      .attr('stroke', 'rgba(255, 0, 0, 0.8)')
+      .attr('stroke-width', 2);
+    
+    legend.append('text')
+      .attr('x', 10)
+      .attr('y', 20)
+      .style('font-size', '12px')
+      .text('Median');
+    
+    legend.append('line')
+      .attr('x1', -5)
+      .attr('y1', 15)
+      .attr('x2', 8)
+      .attr('y2', 15)
+      .attr('stroke', 'rgba(135, 0, 255, 0.8)')
+      .attr('stroke-width', 2);
+    
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+function groupDataByDate(data) {
+  const groupedData = {};
+  data.forEach(datum => {
+    const date = datum.x.toDateString();
+    if (!groupedData[date]) {
+      groupedData[date] = [];
+    }
+    groupedData[date].push(datum.y);
+  });
+  return groupedData;
+}
---
+++ 3060scraper.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+
+# Define the scope and credentials
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+
+# Authorize the credentials
+client = gspread.authorize(credentials)
+
+# Open the Google Sheet
+sheet_url = 'https://docs.google.com/spreadsheets/d/1Bzedq9WqMuvgaV9VdpGXVJ4kXEjC5iVpslgnMFNYMkk/edit#gid=0'
+sheet = client.open_by_url(sheet_url).sheet1
+
+
+
+# Get the existing data from the Google Sheet
+existing_data = sheet.get_all_values()
+
+# Determine the starting row for appending new data
+starting_row = len(existing_data) + 1
+
+n = 1
+
+prices_list = []  # Empty list to store prices
+result_numbers_list = []  # Empty list to store result numbers
+links_list = []
+
+url = "https://www.newegg.com/p/pl?N=4814%20601361654%20100007709&d=3060&Order=3"
+
+result = requests.get(url).text
+doc = BeautifulSoup(result, "html.parser")
+
+prices = doc.find_all("li", class_="price-current", limit=25)
+links = doc.find_all("a", class_="item-title", limit=25)
+
+
+for i in range(len(prices)):
+    strong = prices[i].find("strong")
+    sup = prices[i].find("sup")
+
+    if strong and sup:
+        price_value = strong.text.replace(",", "") + sup.text.replace(",", "")
+        prices_list.append(price_value)
+        result_numbers_list.append(n)
+        link = links[i]['href']
+        links_list.append(link)
+        print("$" + price_value)
+        print("Link:", link)
+    else:
+        continue
+
+    print("Result number =", n)
+    print("\n")
+    n += 1
+    if n > 14:
+        break
+
+# Get today's date
+today = datetime.date.today()
+
+# Convert the date components to strings
+month = str(today.month)
+year = str(today.year)
+day = str(today.day)
+
+# Prepare data for updating the sheet
+data = []
+for i in range(len(result_numbers_list)):
+    if i < len(prices_list):
+        
+        data.append([links_list[i], str(month + "/" + day + "/" + year), prices_list[i], "3060 GPU's"])
+    else:
+        data.append([links_list[i], "Price information not found"])
+
+# Remove the last entry if it is blank
+if data[-1][1] == '':
+    data.pop()
+
+# Update the sheet by appending new data
+sheet.update(f'A{starting_row}:D', data)
